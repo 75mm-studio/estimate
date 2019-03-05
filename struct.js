b34032ca845586995f70455fcaa65e26a1ca1b2c
kim han woong <khw7096@gmail.com>
2019-03-05 14:39:16 +0900

    1000.0 모델을 추가함.

--- struct.js
+++ struct.js
@@ -1,9 +1,10 @@
-var items = [];
+var shots = [];
 
-const item = {
+const shot = {
 	// currentTotal = initAmount * count * attributeRates
 	"initAmount" : 300000.0,
-	"count" : 1,
+	"frameAmount" : 1000.0,
+	"frame" : 1,
 	"attributes" : [], // attribute list
 };
 
@@ -20,16 +21,16 @@ a = Object.create(attribute)
 a.name = "featureFilm";
 a.type = "project";
 a.rate = 1.0;
-item.attributes.push(a)
+shot.attributes.push(a)
 
 // anamorphic lens 추가함.
 b = Object.create(attribute)
 b.name = "anamorphic lens";
 b.type = "lens";
 b.rate = 2.0;
-item.attributes.push(b)
+shot.attributes.push(b)
 
 
-console.log(item.attributes);
-items.push(item);
-console.log(items);
+console.log(shot.attributes);
+shots.push(shot);
+console.log(shots);
