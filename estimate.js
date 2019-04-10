77b3166b0931f7459b9c6be36c0b16f301f024cc
kim hanwoong <khw7096@gmail.com>
2019-04-10 21:40:44 +0900

    견적서 추가함.

--- estimate.js
+++ estimate.js
@@ -1,116 +1,134 @@
 var bucket = [];
-var initAmount = 300000.0;
 
-const shot = {
-	// currentTotal = initAmount * shotnum * attributeRates
+const shotStruct = {
 	"initAmount" : 300000.0,
-	"shotnum" : 1,
+	"num" : 1,
 	"attributes" : [], // attribute list
 };
 
-const attribute = {
+const attributeStruct = {
 	"id":"",
-	"type":"", // camera, lens, scripts ...
-	"rate":1.0,
+	"name":"", // camera, lens, scripts ...
+	"value":1.0,
 };
 
 // addBucket
 document.getElementById('addBucket').addEventListener('click', addBucket);
 
 function addBucket() {
-	// projectOption
-	var currentProjectOption = 1.0;
-	var projectOption = document.getElementsByName("projectOption");
-	for (var i = 0, length = projectOption.length; i < length; i++) {
-		if (projectOption[i].checked) {
-			currentProjectOption = projectOption[i].value;
+	shot = Object.create(shotStruct);
+	shot.attributes = []; // 기존의 Attrbute를 초기화 한다.
+	// project
+	var currentProject = 1.0;
+	var project = document.getElementsByName("project");
+	for (var i = 0, length = project.length; i < length; i++) {
+		if (project[i].checked) {
+			currentProject = project[i].value;
+			attr = Object.create(attributeStruct);
+			attr.id = project[i].id;
+			attr.name = project[i].name;
+			attr.value = project[i].value;
+			shot.attributes.push(attr)
 			break;
 		};
 	}
-	// lensOption
-	var currentLensOption = 1.0;
-	var lensOption = document.getElementsByName("lensOption");
-	for (var i = 0, length = lensOption.length; i < length; i++) {
-		if (lensOption[i].checked) {
-			currentLensOption = lensOption[i].value;
+	// lens
+	var currentLens = 1.0;
+	var lens = document.getElementsByName("lens");
+	for (var i = 0, length = lens.length; i < length; i++) {
+		if (lens[i].checked) {
+			currentLens = lens[i].value;
+			attr = Object.create(attributeStruct);
+			attr.id = lens[i].id;
+			attr.name = lens[i].name;
+			attr.value = lens[i].value;
+			shot.attributes.push(attr)
 			break;
 		};
 	}
-	// stereoOption
-	var currentStereoOption = 1.0;
-	var stereoOption = document.getElementsByName("stereoOption");
-	for (var i = 0, length = stereoOption.length; i < length; i++) {
-		if (stereoOption[i].checked) {
-			currentStereoOption = stereoOption[i].value;
+	// multicam
+	var currentMulticam = 1.0;
+	var multicam = document.getElementsByName("multicam");
+	for (var i = 0, length = multicam.length; i < length; i++) {
+		if (multicam[i].checked) {
+			currentMulticam = multicam[i].value;
+			attr = Object.create(attributeStruct);
+			attr.id = multicam[i].id;
+			attr.name = multicam[i].name;
+			attr.value = multicam[i].value;
+			shot.attributes.push(attr)
 			break;
 		};
 	}
-	// serviceOption
-	var currentServiceOption = 1.0;
-	var serviceOptions = document.getElementsByName("serviceOption");
-	for (var i = 0, length = serviceOptions.length; i < length; i++) {
-		if (serviceOptions[i].checked) {
-			currentServiceOption *= serviceOptions[i].value;
+	// service
+	var currentService = 1.0;
+	var service = document.getElementsByName("service");
+	for (var i = 0, length = service.length; i < length; i++) {
+		if (service[i].checked) {
+			currentService *= service[i].value;
+			attr = Object.create(attributeStruct);
+			attr.id = service[i].id;
+			attr.name = service[i].name;
+			attr.value = service[i].value;
+			shot.attributes.push(attr)
 		};
 	}
 
-	// serviceDetailOption
-	var currentServiceDetailOption = 1.0;
-	var serviceDetailOption = document.getElementsByName("serviceDetailOption");
-	for (var i = 0, length = serviceDetailOption.length; i < length; i++) {
-		if (serviceDetailOption[i].checked) {
-			currentServiceDetailOption = serviceDetailOption[i].value;
+	// serviceDetail
+	var currentServiceDetail = 1.0;
+	var serviceDetail = document.getElementsByName("serviceDetail");
+	for (var i = 0, length = serviceDetail.length; i < length; i++) {
+		if (serviceDetail[i].checked) {
+			currentServiceDetail = serviceDetail[i].value;
+			attr = Object.create(attributeStruct);
+			attr.id = serviceDetail[i].id;
+			attr.name = serviceDetail[i].name;
+			attr.value = serviceDetail[i].value;
+			shot.attributes.push(attr)
 			break;
 		};
 	}
-	// vfxScriptOption
-	var currentVfxScriptOption = 1.0;
-	var vfxScriptOptions = document.getElementsByName("vfxScriptOption");
-	for (var i = 0, length = vfxScriptOptions.length; i < length; i++) {
-		if (vfxScriptOptions[i].checked) {
-			currentVfxScriptOption *= vfxScriptOptions[i].value;
+	// vfxScript
+	var currentVfxScript = 1.0;
+	var vfxScripts = document.getElementsByName("vfxScript");
+	for (var i = 0, length = vfxScripts.length; i < length; i++) {
+		if (vfxScripts[i].checked) {
+			currentVfxScript *= vfxScripts[i].value;
+			attr = Object.create(attributeStruct);
+			attr.id = vfxScripts[i].id;
+			attr.name = vfxScripts[i].name;
+			attr.value = vfxScripts[i].value;
+			shot.attributes.push(attr)
 		};
 	}
 
 	// reconstructOption
-	var currentReconstructOption = 1.0;
-	var reconstructOptions = document.getElementsByName("reconstructOption");
-	for (var i = 0, length = reconstructOptions.length; i < length; i++) {
-		if (reconstructOptions[i].checked) {
-			currentReconstructOption *= reconstructOptions[i].value;
+	var currentReconstruct = 1.0;
+	var reconstructs = document.getElementsByName("reconstruct");
+	for (var i = 0, length = reconstructs.length; i < length; i++) {
+		if (reconstructs[i].checked) {
+			currentReconstruct *= reconstructs[i].value;
+			attr = Object.create(attributeStruct);
+			attr.id = reconstructs[i].id;
+			attr.name = reconstructs[i].name;
+			attr.value = reconstructs[i].value;
+			shot.attributes.push(attr)
 		};
 	}
 
 	// number of shot
 	var nos = document.getElementById("numberOfShot").value;
-	initAmount *= currentProjectOption;
-	initAmount *= currentLensOption;
-	initAmount *= currentStereoOption;
-	initAmount *= currentServiceOption;
-	initAmount *= currentServiceDetailOption;
-	initAmount *= currentVfxScriptOption;
-	initAmount *= currentReconstructOption;
+	shot.num = parseFloat(nos);
+	initAmount = 300000.0;
+	initAmount *= currentProject;
+	initAmount *= currentLens;
+	initAmount *= currentMulticam;
+	initAmount *= currentService;
+	initAmount *= currentServiceDetail;
+	initAmount *= currentVfxScript;
+	initAmount *= currentReconstruct;
 	initAmount *= parseFloat(nos);
 	console.log(initAmount);
-	initAmount = 300000.0
+	bucket.push(shot)
+	console.log(bucket);
 }
-
-
-// featureFilm 추가함.
-a = Object.create(attribute)
-a.id = "featureFilm";
-a.type = "project";
-a.rate = 1.0;
-shot.attributes.push(a)
-
-// anamorphic lens 추가함.
-b = Object.create(attribute)
-b.id = "anamorphic lens";
-b.type = "lens";
-b.rate = 2.0;
-shot.attributes.push(b)
-
-
-console.log(shot.attributes);
-bucket.push(shot);
-console.log(bucket);
