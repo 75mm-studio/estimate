e8d0b466dcd932d8959f7644445cf72bf8115a73
woong <woong@lazypic.org>
2020-02-22 22:50:49 +0900

    계산기의 +,= 키가 물리적으로 작동할 수 있도록 코드 변경.

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,30 @@
 // dis함수는 계산기의 디스플레이틀 업데이트 한다.
 function dis(val) { 
     document.getElementById("calHistory").innerText += val
+
+    // 계산기의 결과값에 오직 0만 있다면 빈문자열로 만들고 시작한다.
+    if (document.getElementById("calResult").innerText === '0') {
+        document.getElementById("calResult").innerText = "";
+    }
+    // 계산기 History에 +가 없다는 것은 최초 숫자만 있다는 뜻이다.
+    // + 가 존재하는지 체크하고 없을때 calResult에 입력숫자를 넣는다.
+    if (!document.getElementById("calHistory").innerText.includes("+")) {
+        document.getElementById("calResult").innerText += val
+    }
+}
+
+// add 함수는 계산기에서 + 버튼을 눌렀을 때 실행되는 함수이다.
+function add() { 
+    document.getElementById("calResult").innerText = eval(document.getElementById("calHistory").innerText);
+    document.getElementById("calHistory").innerText += "+"
 }
 
 // clr 함수는 계산기에서 C 버튼을 눌렀을 때 실행되는 함수이다.
 function clr() {
     // 기존 총 프레임수를 초기화 한다.
     document.getElementById("totalFrame").innerText = "0";
-    // 계산기의 결과를 초기화 한다.
+    // 계산기의 히스토리를 초기화 한다.
     document.getElementById("calHistory").innerText = "";
+    // 계산기의 결과를 초기화 한다.
+    document.getElementById("calResult").innerText = "0";
 } 
