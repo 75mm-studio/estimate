05f34ed7e65cbcb51ef8d6f7ec1936f621e3d0fa
woong <woong@lazypic.org>
2020-03-06 15:09:21 +0900

    edit calculator

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-aws s3 cp ./estimate.html s3://75mm.studio --acl public-read --profile 75mm.studio
+aws s3 cp ./calculator.html s3://75mm.studio --acl public-read --profile 75mm.studio
 aws s3 cp ./js/calculator.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
 aws s3 cp ./js/estimate.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
 aws s3 cp ./css/estimate.css s3://75mm.studio/css/ --acl public-read --profile 75mm.studio
