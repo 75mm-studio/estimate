d10efc9f694823f0e23af69b75d7cdd0b61d7410
Bailey <bsy2658@gmail.com>
2020-06-12 14:49:26 +0900

    frameNum2Cost 수정

--- js/estimate_eng.js
+++ js/estimate_eng.js
@@ -347,13 +347,31 @@ function setInputFilter(textbox, inputFilter) {
 
 //프레임 개수에 따라 가중치를 고려해 가격을 반환하는 함수.
 function frameNum2Cost(num){
-    if(num <= 500){
-        return 1000*num;
-    }else if(num <= 1000){
-        return 3000*num - 1000000;
-    }else if(num <= 2000){
-        return 4000*num - 2000000;
-    }
+	if(num < 300){ // 300미만
+		return 1000*num
+	}else if(num < 600){ // 600미만
+		return 1100*num
+	}else if(num < 700){
+		return 1200*num
+	}else if(num < 800){
+		return 1400*num
+	}else if(num < 900){
+		return 1600*num
+	}else if(num < 1000){
+		return 1800*num
+	}else if(num < 1200){
+		return 2000*num
+	}else if(num < 1400){
+		return 2200*num
+	}else if(num < 1600){
+		return 2400*num
+	}else if(num < 1800){
+		return 2600*num
+	}else if(num < 2000){
+		return 2800*num
+	}else{
+		return 3000*num
+	}
 }
 
 // Install input filters.
